b85e66fcec1bce7e6bbda0fa47c94d3ac7bb9713
Ralph Flachsbart <ralphflachs@gmail.com>
2024-09-05 16:55:37 -0700

    Contact base class

---
+++ Models/Contact.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendorRad.Models
+{
+    public class Contact
+    {
+        public string Name { get; set; }
+        public string Company { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+    }
+}
