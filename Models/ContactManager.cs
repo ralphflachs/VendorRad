563d56d9fe48529f2a6f5048e9b93bff03f37a3a
Ralph Flachsbart <ralphflachs@gmail.com>
2024-09-06 10:54:20 -0700

    Check contact against list before adding#

--- Models/ContactManager.cs
+++ Models/ContactManager.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.IO;
+using System.Linq;
 using System.Text.Json;
 
 namespace VendorRad.Models
@@ -12,13 +10,35 @@ namespace VendorRad.Models
     {
         private readonly string filePath = "contacts.json";
 
-        // Save contact with file locking
+        // Save contact with file locking (update if exists, add if new)
         public void SaveContact(Contact contact)
         {
             lock (filePath)
             {
                 var contacts = LoadContacts();
-                contacts.Add(contact);
+
+                // Try to find if the contact already exists based on unique criteria (e.g., Name or PhoneNumber)
+                var existingContact = contacts.FirstOrDefault(c => c.Name == contact.Name || c.PhoneNumber == contact.PhoneNumber);
+
+                if (existingContact != null)
+                {
+                    // Update existing contact details
+                    existingContact.Company = contact.Company;
+                    existingContact.Address = contact.Address;
+                    existingContact.PhoneNumber = contact.PhoneNumber;
+
+                    if (existingContact is Customer existingCustomer && contact is Customer newCustomer)
+                    {
+                        existingCustomer.SalesNotes = newCustomer.SalesNotes; // Update Customer-specific field
+                    }
+                }
+                else
+                {
+                    // If contact doesn't exist, add it as a new contact
+                    contacts.Add(contact);
+                }
+
+                // Save updated contact list
                 SaveContacts(contacts);
             }
         }
@@ -46,5 +66,4 @@ namespace VendorRad.Models
             }
         }
     }
-
 }
