c3151ca97f2e5cccde4026935f8bcf555d2c4de4
Ralph Flachsbart <ralphflachs@gmail.com>
2024-09-05 17:07:43 -0700

    Vendor and customer models

---
+++ Models/Customer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendorRad.Models
+{
+    public class Customer : Contact
+    {
+        public string SalesNotes { get; set; }
+    }
+}
