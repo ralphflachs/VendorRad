751667290b682edea601d970eab8fe097a3a35e4
Ralph Flachsbart <ralphflachs@gmail.com>
2024-09-07 11:12:09 -0700

    Vendor company model

---
+++ Models/MasterVendor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendorRad.Models
+{
+    public class MasterVendor
+    {
+        public required string CompanyName { get; set; }
+        public required string VendorCode { get; set; }
+    }
+
+}
