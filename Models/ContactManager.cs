8faf0930a9d55a166bebae3e6ef2a28bc2e848e1
Ralph Flachsbart <ralphflachs@gmail.com>
2024-09-07 11:37:21 -0700

    Maintain separate vendor list

--- Models/ContactManager.cs
+++ Models/ContactManager.cs
@@ -1,23 +1,30 @@
 ﻿using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Text.Json;
 
 namespace VendorRad.Models
 {
     public class ContactManager
     {
-        private readonly string filePath = "contacts.json";
+        private readonly string contactsFilePath = "contacts.json";
+        private readonly string masterVendorFilePath = "master_vendors.json";
+
+        public List<MasterVendor> MasterVendors { get; private set; }
+
+        public ContactManager()
+        {
+            LoadMasterVendors();
+        }
 
         // Load contacts from the file
         public List<Contact> LoadContacts()
         {
-            if (!File.Exists(filePath))
+            if (!File.Exists(contactsFilePath))
             {
                 return new List<Contact>();
             }
 
-            var json = File.ReadAllText(filePath);
+            var json = File.ReadAllText(contactsFilePath);
             return JsonSerializer.Deserialize<List<Contact>>(json) ?? new List<Contact>();
         }
 
@@ -26,9 +33,33 @@ namespace VendorRad.Models
         {
             var json = JsonSerializer.Serialize(contacts, new JsonSerializerOptions { WriteIndented = true });
 
-            lock (filePath)
+            lock (contactsFilePath)
             {
-                File.WriteAllText(filePath, json);
+                File.WriteAllText(contactsFilePath, json);
+            }
+        }
+
+        // Load master vendor list from the file
+        public void LoadMasterVendors()
+        {
+            if (!File.Exists(masterVendorFilePath))
+            {
+                MasterVendors = new List<MasterVendor>();
+                return;
+            }
+
+            var json = File.ReadAllText(masterVendorFilePath);
+            MasterVendors = JsonSerializer.Deserialize<List<MasterVendor>>(json) ?? new List<MasterVendor>();
+        }
+
+        // Save master vendors to the file
+        public void SaveMasterVendors(List<MasterVendor> masterVendors)
+        {
+            var json = JsonSerializer.Serialize(masterVendors, new JsonSerializerOptions { WriteIndented = true });
+
+            lock (masterVendorFilePath)
+            {
+                File.WriteAllText(masterVendorFilePath, json);
             }
         }
     }
