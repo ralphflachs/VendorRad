3cebac6fe300e874a71d3bb8878f061d4ea12cc1
Ralph Flachsbart <ralphflachs@gmail.com>
2024-09-07 11:27:17 -0700

    Revert "Maintain separate master vendor list"
    This reverts commit f8df74446a79c7a16848ce7591fcabcab540c084.

--- Models/ContactManager.cs
+++ Models/ContactManager.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Text.Json;
@@ -8,89 +7,28 @@ namespace VendorRad.Models
 {
     public class ContactManager
     {
-        private readonly string contactsFilePath = "contacts.json";
-        private readonly string masterVendorsFilePath = "master_vendors.json";
-
-        public ObservableCollection<Contact> Contacts { get; private set; }
-        public List<MasterVendor> MasterVendors { get; private set; }
-
-        public ContactManager()
-        {
-            Contacts = new ObservableCollection<Contact>();
-            MasterVendors = new List<MasterVendor>();
-            LoadContacts();
-            LoadMasterVendors();
-        }
+        private readonly string filePath = "contacts.json";
 
         // Load contacts from the file
-        public void LoadContacts()
+        public List<Contact> LoadContacts()
         {
-            if (!File.Exists(contactsFilePath))
+            if (!File.Exists(filePath))
             {
-                Contacts = new ObservableCollection<Contact>();
-                return;
+                return new List<Contact>();
             }
 
-            var json = File.ReadAllText(contactsFilePath);
-            var contactsFromFile = JsonSerializer.Deserialize<List<Contact>>(json) ?? new List<Contact>();
-            Contacts = new ObservableCollection<Contact>(contactsFromFile);
-        }
-
-        // Load master vendors from the file
-        public void LoadMasterVendors()
-        {
-            if (!File.Exists(masterVendorsFilePath))
-            {
-                MasterVendors = new List<MasterVendor>();
-                return;
-            }
-
-            var json = File.ReadAllText(masterVendorsFilePath);
-            MasterVendors = JsonSerializer.Deserialize<List<MasterVendor>>(json) ?? new List<MasterVendor>();
-        }
-
-        public bool AddVendor(Vendor vendor, string companyName, string vendorCode)
-        {
-            // Check if the company exists in the master list
-            var masterVendor = MasterVendors.FirstOrDefault(mv => mv.CompanyName == companyName);
-
-            if (masterVendor == null)
-            {
-                // If the company doesn't exist, create a new MasterVendor and add it to the list
-                masterVendor = new MasterVendor { CompanyName = companyName, VendorCode = vendorCode };
-                MasterVendors.Add(masterVendor);
-                SaveMasterVendors();
-            }
-
-            // Now link the vendor contact to the corresponding MasterVendor
-            vendor.MasterVendor = masterVendor;
-
-            // Save the vendor contact
-            Contacts.Add(vendor);
-            SaveContacts();
-
-            return true;
+            var json = File.ReadAllText(filePath);
+            return JsonSerializer.Deserialize<List<Contact>>(json) ?? new List<Contact>();
         }
 
         // Save contacts to the file
-        public void SaveContacts()
+        public void SaveContacts(List<Contact> contacts)
         {
-            var json = JsonSerializer.Serialize(Contacts.ToList(), new JsonSerializerOptions { WriteIndented = true });
+            var json = JsonSerializer.Serialize(contacts, new JsonSerializerOptions { WriteIndented = true });
 
-            lock (contactsFilePath)
+            lock (filePath)
             {
-                File.WriteAllText(contactsFilePath, json);
-            }
-        }
-
-        // Save master vendors to the file
-        public void SaveMasterVendors()
-        {
-            var json = JsonSerializer.Serialize(MasterVendors, new JsonSerializerOptions { WriteIndented = true });
-
-            lock (masterVendorsFilePath)
-            {
-                File.WriteAllText(masterVendorsFilePath, json);
+                File.WriteAllText(filePath, json);
             }
         }
     }
