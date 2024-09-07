f0f73ada2998ef84b306e523e6775ed6620e2533
Ralph Flachsbart <ralphflachs@gmail.com>
2024-09-07 11:47:35 -0700

    Avoid duplicating vendor company

--- MainViewModel.cs
+++ MainViewModel.cs
@@ -6,14 +6,20 @@ namespace VendorRad.ViewModels
     public class MainViewModel
     {
         public ObservableCollection<Contact> Contacts { get; set; }
+        public ObservableCollection<MasterVendor> MasterVendors { get; set; }
 
         private readonly ContactManager contactManager;
 
         public MainViewModel()
         {
             contactManager = new ContactManager();
+
+            // Load contacts and master vendors from the ContactManager
             var contactsFromManager = contactManager.LoadContacts();
+            var masterVendorsFromManager = contactManager.MasterVendors;
+
             Contacts = new ObservableCollection<Contact>(contactsFromManager);
+            MasterVendors = new ObservableCollection<MasterVendor>(masterVendorsFromManager);
         }
 
         // Add contact in the ObservableCollection
@@ -21,6 +27,30 @@ namespace VendorRad.ViewModels
         {
             Contacts.Add(contact);
             contactManager.SaveContacts([.. Contacts]);
+        }
+
+        // Add or update a vendor in the ObservableCollection and master vendor list
+        public bool AddVendor(Vendor vendor, string companyName, string vendorCode)
+        {
+            // Check if the company exists in the master vendor list
+            var masterVendor = MasterVendors.FirstOrDefault(mv => mv.CompanyName == companyName);
+
+            if (masterVendor == null)
+            {
+                // If the company doesn't exist, add it to the master vendor list
+                masterVendor = new MasterVendor { CompanyName = companyName, VendorCode = vendorCode };
+                MasterVendors.Add(masterVendor);
+                contactManager.SaveMasterVendors(MasterVendors.ToList());
+            }
+
+            // Link the vendor contact to the corresponding MasterVendor
+            vendor.MasterVendor = masterVendor;
+
+            // Add new vendor
+            Contacts.Add(vendor);
+            contactManager.SaveContacts([.. Contacts]);
+
+            return true;
         }
     }
 }
